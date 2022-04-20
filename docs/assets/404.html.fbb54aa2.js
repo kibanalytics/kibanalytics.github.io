2d1d1f2358df7ebc5d6b173a61fcbf0b1215bff5
ibrahim-netto <ibrahim.netto@outlook.com>
2022-04-20 00:14:27 -0300

    - Docs deployment test

--- docs/assets/404.html.fbb54aa2.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./app.0e5f0c9f.js";const _={};function r(t,s){return null}var c=e(_,[["render",r],["__file","/a/kibanalytics/docs/.vuepress/.temp/pages/404.html.vue"]]);export{c as default};
